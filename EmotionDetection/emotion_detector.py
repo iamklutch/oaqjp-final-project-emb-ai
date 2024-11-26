2509490b3fc517d60103700024bc21c263927cbf
James Higashiyama <james.higashiyama@gmail.com>
2024-11-26 16:38:13 -0700

    Update emotion_detector.py

--- EmotionDetection/emotion_detector.py
+++ EmotionDetection/emotion_detector.py
@@ -1,24 +1,29 @@
 import requests
 import json
 
-def emotion_detector(text_to_analyze):
-    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    input_json = { "raw_document": { "text": text_to_analyze } }
-    response = requests.post(URL, json = input_json, headers=header)
-    formated_response = json.loads(response.text)
+def emotion_detector(text_to_analyse):
 
-    if response.status_code == 200:
-        return formated_response
-    elif response.status_code == 400:
-        formated_response = {
-                            'anger': None,
-                            'disgust': None, 
-                            'fear': None, 
-                            'joy': None, 
-                            'sadness': None, 
-                            'dominant_emotion': None}
-        return formated_response
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = {"raw_document": { "text": text_to_analyse } }
+    response = requests.post(url, json = myobj, headers = headers)
+    status_code = response.status_code
+    
+    if status_code == 400:
+        formatted_output = { 'anger': None,
+                             'disgust': None,
+                             'fear': None,
+                             'joy': None,
+                             'sadness': None,
+                             'dominant_emotion': None }
+    else:
+        res = json.loads(response.text)
+        formatted_output = res[‘emotionPredictions’][0][‘emotion’]
+        dominant_emotion = max(formatted_response, key = lambda x: formatted_response[x])
+        formatted_response[‘dominant_dictionary’] = dominant_emotion
+
+    return formatted_response 
+
 
 def emotion_predictor(detected_text):
     if all(value is None for value in detected_text.values()):
